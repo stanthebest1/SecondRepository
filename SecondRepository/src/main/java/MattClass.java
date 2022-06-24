cc60715227404a22af7b859c41da23030e730aa6
msinnemaki <matt.sinnemaki@gmail.com>
2022-06-24 08:47:13 -0600

    adding Matt Branch

---
+++ SecondRepository/src/main/java/MattClass.java
@@ -0,0 +1,15 @@
+public class MattClass
+{
+    public MattClass ()
+    {
+
+    }
+
+    public void NewMattClass ()
+    {
+        for (int i =0; i < 4; i++)
+        {
+            System.out.println("Hello World");
+        }
+    }
+}
